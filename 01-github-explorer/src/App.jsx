af3231342b38ba9ee431824c69a0bc55fb8a5093
hugomendonca98 <hugomendonca9@gmail.com>
2022-08-15 19:43:23 -0300

    :sparkles: Primeiro componente React

--- 01-github-explorer/src/App.jsx
+++ 01-github-explorer/src/App.jsx
@@ -1,7 +1,9 @@
 import './styles/global.scss';
+import RepositoryList from './components/RepositoryList';
+
 
 export default function App() {
     return (
-        <div>Hello</div>
+        <RepositoryList />
     )
 }
---
+++ 01-github-explorer/src/components/RepositoryList/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const RepositoryList = () => {
+    return (
+        <section className='repository-list'>
+            <h1>Lista de repositórios</h1>
+            <ul>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+                    <a href="#">Acessar repositório</a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+                    <a href="#">Acessar repositório</a>
+                </li>
+            </ul>
+        </section>
+    )
+}
+
+export default RepositoryList;
