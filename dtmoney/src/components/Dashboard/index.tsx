5e88133cf239ae9dd48221d20bf913be52452f02
hugomendonca98 <hugomendonca9@gmail.com>
2022-08-31 17:11:47 -0300

    :sparkles: Componente: TransactionsTable

--- dtmoney/src/components/Dashboard/index.tsx
+++ dtmoney/src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from "../Summary"
+import { TransationTable } from "../TransationTable"
 import { Container } from "./styles"
 
 export const Dashboard = () => {
   return (
     <Container>
         <Summary />
+        <TransationTable />
     </Container>
   )
 }
