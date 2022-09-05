7201fd48037092bb43f3b8150179c305ec611a43
hugomendonca98 <hugomendonca9@gmail.com>
2022-09-04 21:49:43 -0300

    :bug: Fix gap TransactionType buttons

--- dtmoney/src/components/NewTransactionModal/styles.ts
+++ dtmoney/src/components/NewTransactionModal/styles.ts
@@ -50,7 +50,7 @@ export const TransactionTypeContainer = styled.div`
     margin: 1rem 0;
     display: grid;
     grid-template-columns: 1fr 1fr;
-    gap: 0.5;
+    gap: 0.5rem;
 
     button {
         height: 4rem;
