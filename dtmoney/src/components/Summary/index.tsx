2b29a4682b70715bd6dcc5da73f53435d43376c3
hugomendonca98 <hugomendonca9@gmail.com>
2022-09-07 15:06:48 -0300

    :sparkles: Calculando resumo

--- dtmoney/src/components/Summary/index.tsx
+++ dtmoney/src/components/Summary/index.tsx
@@ -3,14 +3,31 @@ import { Container } from "./styles"
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import totalImg from '../../assets/total.svg';
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import { TransactionsContext } from "../../TransactionContext";
 
 export const Summary = () => {
 
   const { transactions } = useContext(TransactionsContext);
 
-  console.log(transactions);
+  const summary = useMemo(()=> {
+    return transactions.reduce((acc, transaction)=> {
+      if(transaction.type === 'deposit'){
+        acc.deposits += transaction.amount;
+        acc.total += transaction.amount;
+      }else {
+        acc.withdraw += transaction.amount;
+        acc.total -= transaction.amount;
+      }
+
+      return acc;
+
+    }, {
+      deposits: 0,
+      withdraw: 0,
+      total: 0,
+    });
+  }, [transactions]);
 
   return (
     <Container>
@@ -19,21 +36,30 @@ export const Summary = () => {
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas" />
         </header>
-        <strong>R$1000,00</strong>
+        <strong>{new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(summary.deposits)}</strong>
       </div>
       <div>
         <header>
           <p>Saidas</p>
           <img src={outcomeImg} alt="Saidas" />
         </header>
-        <strong>- R$500,00</strong>
+        <strong>-{new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(summary.withdraw)}</strong>
       </div>
       <div className="highlight-background">
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Total" />
         </header>
-        <strong>R$500,00</strong>
+        <strong>{new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(summary.total)}</strong>
       </div>
     </Container>
   )
