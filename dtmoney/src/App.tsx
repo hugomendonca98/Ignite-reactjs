21a7e781961d0550f1d1e88f92e6a1af522f01c9
hugomendonca98 <hugomendonca9@gmail.com>
2022-09-07 13:00:42 -0300

    :sparkles: Movendo criação para o context

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,12 +1,10 @@
 import { useCallback, useState } from "react";
 
-
 import { GlobalStyle } from "./styles/global";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
-
-
+import { TransactionsProvider } from "./TransactionContext";
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
@@ -20,7 +18,7 @@ export function App() {
   }, []);
 
   return (
-    <>
+    <TransactionsProvider>
       <GlobalStyle />
       <NewTransactionModal 
         isNewTransactionModalOpen={isNewTransactionModalOpen}
@@ -28,6 +26,6 @@ export function App() {
       />
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
-    </>
+    </TransactionsProvider>
   );
 }
--- dtmoney/src/styles/global.ts
+++ dtmoney/src/styles/global.ts
@@ -18,7 +18,7 @@ export const GlobalStyle = createGlobalStyle`
         padding: 0;
         box-sizing: border-box;
     }
-
+    
     html {
         @media (max-width: 1388px) {
             font-size: 93.75%;
