207caa44c8866bc9021c31615b41ed9e8d6c766f
hugomendonca98 <hugomendonca9@gmail.com>
2022-09-19 09:54:41 -0300

    :sparkles: Página Home

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,11 +1,32 @@
 import Head from "next/head";
+import Image from "next/image";
+
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | Ignews</title>
+        <title>Home | Ignews</title>
       </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <Image
+          src="/images/avatar.svg"
+          alt="Girl coding"
+          width="334"
+          height="520"
+        />
+      </main>
     </>
   );
 }
