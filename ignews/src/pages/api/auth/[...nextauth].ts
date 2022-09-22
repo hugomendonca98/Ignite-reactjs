4ccf793950845543c2a76e7c1d39dfcdf38b9c2a
hugomendonca98 <hugomendonca9@gmail.com>
2022-09-22 17:27:20 -0300

    :sparkles: Salvando usuário no banco

--- ignews/src/pages/api/auth/[...nextauth].ts
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,15 +1,32 @@
+import { query as q } from "faunadb";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { fauna } from "../../../services/fauna";
 
 export const authOptions = {
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
       authorization: { params: { scope: "read:user" } },
     }),
-    // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      try {
+        await fauna.query(
+          q.Create(q.Collection("users"), {
+            data: { email: user.email },
+          })
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+
+      return true;
+    },
+  },
 };
 export default NextAuth(authOptions);
