62a45e3d3d6bf0463f9afd863cbbd7ba78538fbc
Hugo mendonca <hugomendonca9@gmail.com>
2023-12-30 20:31:55 -0300

    Testando página do post

--- ignews/src/pages/posts/[slug].tsx
+++ ignews/src/pages/posts/[slug].tsx
@@ -46,7 +46,7 @@ export const getServerSideProps: GetServerSideProps = async ({
   if (!session?.activeSubscription) {
     return {
       redirect: {
-        destination: `/posts/preview/${slug}`,
+        destination: `/`,
         permanent: false,
       },
     };
