b0699a428726abea33d22fe5186d730c1eda48d5
Hugo mendonca <hugomendonca9@gmail.com>
2023-12-31 00:55:55 -0300

    Dicas de testes assíncronos

---
+++ ignews/src/components/Async/Async.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react'
+
+export default function Async() {
+
+    const [isButtonVisible, setIsButtonVisible] = useState(true);
+
+    useEffect(()=> {
+        setTimeout(()=> {
+            setIsButtonVisible(false);
+        }, 1000)
+    }, [])
+
+    return (
+        <div>
+            <div>Hello World</div>
+            { isButtonVisible && <button>Button</button>}
+        </div>
+    )
+}
